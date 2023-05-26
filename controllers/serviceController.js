d17e05c4f824b2e401a74de0292e878d9028c859
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-05-26 10:59:58 -0300

    :sparkles: feat: feito post e getAll

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,5 +1,46 @@
 const { Recipe: RecipeModel } = require("../models/Recipe");
 
-const serviceController = {};
+const serviceController = {
+  create: async (req, res) => {
+    try {
+      const recipe = {
+        name: req.body.name,
+        ingredients: req.body.ingredients,
+        method: req.body.method,
+        image: req.body.image,
+      };
+
+      const response = await RecipeModel.create(recipe);
+
+      res.status(201).json({
+        response,
+        msg: "Que delícia! Sua receita já foi incluída no nosso caderninho!",
+      });
+    } catch (error) {
+      console.log(`Erro: ${error}`);
+    }
+  },
+
+  getAll: async (req, res) => {
+    try {
+      const allRecipes = await RecipeModel.find();
+
+      res.json(allRecipes);
+    } catch (error) {
+      console.log(`Erro: ${error}`);
+    }
+  },
+
+  getSingle: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const recipe = await RecipeModel.findById(id);
+
+      res.json(recipe);
+    } catch (error) {
+      console.log(`Erro: ${error}`);
+    }
+  },
+};
 
 module.exports = serviceController;
--- routes/services.js
+++ routes/services.js
@@ -2,4 +2,8 @@ const router = require("express").Router();
 
 const serviceController = require("../controllers/serviceController");
 
+router.route("/recipes").post((req, res) => serviceController.create(req, res));
+
+router.route("/recipes").get((req, res) => serviceController.getAll(req, res));
+
 module.exports = router;
