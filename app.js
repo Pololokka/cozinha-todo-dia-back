23b922d7c24bf97d4cbb55df577bec100ba140ba
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-06-05 18:24:17 -0300

    sparkles: configurado .env

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const app = express();
 
 app.use(cors());
--- db/conn.js
+++ db/conn.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
+const dbUSer = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
 
 async function main() {
   try {
     mongoose.set("strictQuery", true);
 
     await mongoose.connect(
-      "mongodb+srv://bulbovas:Fb0oauUTlBrenZ7E@cluster0.y6535ow.mongodb.net/?retryWrites=true&w=majority"
+      `mongodb+srv://${dbUSer}:${dbPass}@cluster0.y6535ow.mongodb.net/?retryWrites=true&w=majority`
     );
 
     console.log("Conectado no Banco de Dados");
